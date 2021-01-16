c285c0165d2d60dff57ebf1a4b1cf84e4f9de63a
Will Lane <williamlane923@gmail.com>
2021-01-16 12:54:12 -0800

    changed percent sus

--- models/models.go
+++ models/models.go
@@ -2,10 +2,10 @@ package models
 
 type DataRes struct {
 	Metrics struct {
-		NumOfSusWords int `json:"numberOfSusWords"`
-		NumOfSusChars int `json:"numberOfSusCharacters"`
-		TotalChars    int `json:"totalNumberOfCharacters"`
-		PercentSus    int `json:"percentageOfSusToTotal"`
+		NumOfSusWords int     `json:"numberOfSusWords"`
+		NumOfSusChars int     `json:"numberOfSusCharacters"`
+		TotalChars    int     `json:"totalNumberOfCharacters"`
+		PercentSus    float32 `json:"percentageOfSusToTotal"`
 	} `json:"metrics"`
 	Content string `json:"content"`
 }
