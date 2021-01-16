3508ecc0de328fffedd1aacb3fcb555e4f8e0064
Will Lane <williamlane923@gmail.com>
2021-01-16 14:24:30 -0800

    updated models to match api

--- models/models.go
+++ models/models.go
@@ -2,10 +2,19 @@ package models
 
 type DataRes struct {
 	Metrics struct {
-		NumOfSusWords int     `json:"numberOfSusWords"`
-		NumOfSusChars int     `json:"numberOfSusCharacters"`
-		TotalChars    int     `json:"totalNumberOfCharacters"`
-		PercentSus    float32 `json:"percentageOfSusToTotal"`
+		Preliminary struct {
+			NumOfSusWords int     `json:"numberOfSusWords"`
+			NumOfSusChars int     `json:"numberOfSusCharacters"`
+			TotalChars    int     `json:"totalNumberOfCharacters"`
+			PercentSus    float32 `json:"percentageOfSusToTotal"`
+		} `json:"preliminary"`
+		Nlp struct {
+			SentimentData struct {
+				Score       float32 `json:"score"`
+				Comparative float32 `json:"comparative"`
+			} `json:"sentimentData"`
+			GenderBias string `json:"genderBias"`
+		} `json:"nlp"`
 	} `json:"metrics"`
 	Content string `json:"content"`
 }
