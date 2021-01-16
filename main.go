884da08bceeed17f5b4ed0e01656c5ebb3ecb572
Will Lane <williamlane923@gmail.com>
2021-01-15 23:18:24 -0800

    started basic gin project as well as ping mongodb

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	ctx    = context.TODO()
+	C, e   = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	Client = C.Database("motif")
+)
+
+func init() {
+	if e != nil {
+		panic(e)
+	}
+	if e := C.Ping(ctx, nil); e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+	}))
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"hello": "world",
+		})
+	})
+	r.Run()
+}
